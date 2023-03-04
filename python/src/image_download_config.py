c3c3636854bde5f0e3ea780448c7f398431bbcfe
Guy Lafleur <kevin.netherton@gov.bc.ca>
2023-03-03 18:27:20 -0800

    Feat/auto report (#7)
    * testing date in variable 2023-03-03/home/kjnether/rfc_proj/nr-rfc-reanalysis/venv/bin/python
    
    * fix: resolve timezone issue

--- python/src/image_download_config.py
+++ python/src/image_download_config.py
@@ -4,7 +4,10 @@ import enum
 import logging
 import os.path
 
+import pytz
+
 LOGGER = logging.getLogger(__name__)
+TZ = pytz.timezone('America/Vancouver')
 
 
 class UrlType(enum.Enum):
@@ -55,8 +58,8 @@ class TempMinConfig(AbstractImageDLConfig):
     url_template = "https://psl.noaa.gov/cgi-bin/mddb2/plot.pl?doplot=1&varID=4609&fileID=0&itype=0&variable=tmin&levelType=Surface&level_units=&level=Surface&timetype=day&fileTimetype=day&year1={t1_year}&month1={t1_month}&day1={t1_day}&hr1=00%20Z&year2={t2_year}&month2={t2_month}&day2={t2_day}&hr2=00%20Z&vectorPlot=0&contourLevel=custom&cint=4&lowr=-20&highr=20&colormap=default&reverseColormap=no&contourlines=1&colorlines=1&contourfill=1&contourlabels=1&removezonal=0&boundary=AllBoundaries&projection=CylindricalEquidistant&region=Custom&area_north=61&area_west=-140&area_east=-112&area_south=45&centerLat=0.0&centerLon=270.0&mapfill=0.png"
     output_image_file = "Daily_MinT_CPC.png"
     url_type = UrlType.IMAGE
-    date1 = datetime.datetime.now() - datetime.timedelta(days=2)
-    date2 = datetime.datetime.now() - datetime.timedelta(days=2)
+    date1 = datetime.datetime.now(TZ) - datetime.timedelta(days=2)
+    date2 = datetime.datetime.now(TZ) - datetime.timedelta(days=2)
     description = 'CPC minimum Temperature'
 
 
@@ -64,29 +67,29 @@ class TempMaxConfig(AbstractImageDLConfig):
     url_template = "https://psl.noaa.gov/cgi-bin/mddb2/plot.pl?doplot=1&varID=53407&fileID=0&itype=0&variable=tmax&levelType=Surface&level_units=&level=Surface&timetype=day&fileTimetype=day&year1={t1_year}&month1={t1_month}&day1={t1_day}&hr1=00%20Z&year2={t2_year}&month2={t2_month}&day2={t2_day}&hr2=00%20Z&vectorPlot=0&contourLevel=custom&cint=4&lowr=-20&highr=40&colormap=default&reverseColormap=no&contourlines=1&colorlines=1&contourfill=1&contourlabels=1&removezonal=0&boundary=AllBoundaries&projection=CylindricalEquidistant&region=Custom&area_north=61&area_west=-140&area_east=-112&area_south=45&centerLat=0.0&centerLon=270.0&mapfill=0.png"
     output_image_file = "Daily_MaxT_CPC.png"
     url_type = UrlType.IMAGE
-    date1 = datetime.datetime.now() - datetime.timedelta(days=2)
-    date2 = datetime.datetime.now() - datetime.timedelta(days=2)
+    date1 = datetime.datetime.now(TZ) - datetime.timedelta(days=2)
+    date2 = datetime.datetime.now(TZ) - datetime.timedelta(days=2)
     description = 'CPC maxium temperature'
 
 class Precip_mddb2_30day(AbstractImageDLConfig):
     url_template = "https://psl.noaa.gov/cgi-bin/mddb2/plot.pl?doplot=1&varID=2781&fileID=0&itype=0&variable=precip&levelType=Surface&level_units=&level=Surface&timetype=day&fileTimetype=day&createAverage=1&year1={t1_year}&month1={t1_month}&day1={t1_day}&hr1=00%20Z&year2={t2_year}&month2={t2_month}&day2={t2_day}&hr2=00%20Z&vectorPlot=0&contourLevel=custom&cint=2&lowr=0&highr=20&colormap=default&reverseColormap=yes&contourlines=1&colorlines=1&contourfill=1&contourlabels=1&removezonal=0&plotUnits=mm%2Fday&boundary=AllBoundaries&projection=CylindricalEquidistant&region=Custom&area_north=61&area_west=-140&area_east=-112&area_south=45&centerLat=0.0&centerLon=270.0&mapfill=0.png"
     output_image_file = "Daily_Precipitation_30-day_mm_per_day_CPC.png"
     url_type = UrlType.IMAGE
-    date1 = datetime.datetime.now() - datetime.timedelta(days=2 + 29)
-    date2 = datetime.datetime.now() - datetime.timedelta(days=2)
+    date1 = datetime.datetime.now(TZ) - datetime.timedelta(days=2 + 29)
+    date2 = datetime.datetime.now(TZ) - datetime.timedelta(days=2)
     description = 'Daily Precipitation - 30 days'
 
 class Precip_mddb2_90day(Precip_mddb2_30day):
     output_image_file = "Daily_Precipitation_90-day_mm_per_day_CPC.png"
-    date1 = datetime.datetime.now() - datetime.timedelta(days=2 + 89)
-    date2 = datetime.datetime.now() - datetime.timedelta(days=2)
+    date1 = datetime.datetime.now(TZ) - datetime.timedelta(days=2 + 89)
+    date2 = datetime.datetime.now(TZ) - datetime.timedelta(days=2)
     description = 'Daily Precipitation - 90 days'
 
 
 class Precip_mddb2_180day(Precip_mddb2_30day):
     output_image_file = "Daily_Precipitation_180-day_mm_per_day_CPC.png"
-    date1 = datetime.datetime.now() - datetime.timedelta(days=2 + 179)
-    date2 = datetime.datetime.now() - datetime.timedelta(days=2)
+    date1 = datetime.datetime.now(TZ) - datetime.timedelta(days=2 + 179)
+    date2 = datetime.datetime.now(TZ) - datetime.timedelta(days=2)
     description = 'Daily Precipitation - 180 days'
 
 
@@ -94,8 +97,8 @@ class Soil_moisture_l1(AbstractImageDLConfig):
     url_template = "https://charts.ecmwf.int/opencharts-api/v1/products/w_soil_moisture/?valid_time={t1_year}-{t1_month}-{t1_day}T00%3A00%3A00Z&base_time={t2_year}-{t2_month}-{t2_day}T00%3A00%3A00Z&area=North+America"
     output_image_file = "Soil_level1_ECMWF.png"
     url_type = UrlType.JSON
-    date1 = datetime.datetime.now()
-    date2 = datetime.datetime.now()
+    date1 = datetime.datetime.now(TZ)
+    date2 = datetime.datetime.now(TZ)
     description = 'Soil Moisture level 1'
 
 
@@ -110,32 +113,32 @@ class WECWF_24hr_config(AbstractImageDLConfig):
     url_template = "https://charts.ecmwf.int/opencharts-api/v1/products/medium-rain-acc/?valid_time={t1_year}-{t1_month}-{t1_day}T00%3A00%3A00Z&base_time={t2_year}-{t2_month}-{t2_day}T00%3A00%3A00Z&projection=opencharts_north_america"
     output_image_file = "24-hour_accumulated_P_ECMWF.png"
     url_type = UrlType.JSON
-    date1 = datetime.datetime.now() + datetime.timedelta(days=1)
-    date2 = datetime.datetime.now()
+    date1 = datetime.datetime.now(TZ) + datetime.timedelta(days=1)
+    date2 = datetime.datetime.now(TZ)
     description = '24 Hour Accumulated Precipitation'
 
 
 class WECWF_48hr_config(WECWF_24hr_config):
     output_image_file = "48-hour_accumulated_P_ECMWF.png"
-    date1 = datetime.datetime.now() + datetime.timedelta(days=2)
+    date1 = datetime.datetime.now(TZ) + datetime.timedelta(days=2)
     description = '48 Hour Accumulated Precipitation'
 
 
 class WECWF_72hr_config(WECWF_24hr_config):
     output_image_file = "72-hour_accumulated_P_ECMWF.png"
-    date1 = datetime.datetime.now() + datetime.timedelta(days=3)
+    date1 = datetime.datetime.now(TZ) + datetime.timedelta(days=3)
     description = '72 Hour Accumulated Precipitation'
 
 
 class WECWF_96hr_config(WECWF_24hr_config):
     output_image_file = "96-hour_accumulated_P_ECMWF.png"
-    date1 = datetime.datetime.now() + datetime.timedelta(days=4)
+    date1 = datetime.datetime.now(TZ) + datetime.timedelta(days=4)
     description = '96 Hour Accumulated Precipitation'
 
 
 class WECWF_120hr_config(WECWF_24hr_config):
     output_image_file = "120-hour_accumulated_P_ECMWF.png"
-    date1 = datetime.datetime.now() + datetime.timedelta(days=5)
+    date1 = datetime.datetime.now(TZ) + datetime.timedelta(days=5)
     description = '120 Hour Accumulated Precipitation'
 
 
@@ -143,8 +146,8 @@ class NCEP_Soil_moisture_0to10cm_config(AbstractImageDLConfig):
     url_template = "https://psl.noaa.gov/cgi-bin/mddb2/plot.pl?doplot=1&varID=156115&fileID=0&itype=0&variable=soilw&levelType=Between%200-10%20cm%20BGL&level_units=&level=Between%200-10%20cm%20BGL&timetype=4x&fileTimetype=4x&year1={t1_year}&month1={t1_month}&day1={t1_day}&hr1=00%20Z&year2={t2_year}&month2={t2_month}&day2={t2_day}&hr2=00%20Z&vectorPlot=0&contourLevel=auto&cint=4&lowr=0&highr=200&colormap=default&reverseColormap=yes&contourlines=1&colorlines=1&contourfill=1&contourlabels=1&removezonal=0&boundary=AllBoundaries&projection=CylindricalEquidistant&region=Custom&area_north=61&area_west=-140&area_east=-112&area_south=45&centerLat=0.0&centerLon=270.0&mapfill=0.png"
     output_image_file = "Soil_0-10cm_NCEP1.png"
     url_type = UrlType.IMAGE
-    date1 = datetime.datetime.now() - datetime.timedelta(days=2)
-    date2 = datetime.datetime.now()
+    date1 = datetime.datetime.now(TZ) - datetime.timedelta(days=2)
+    date2 = datetime.datetime.now(TZ)
     description = 'Soil Moisture 0 - 10 cm'
 
 
